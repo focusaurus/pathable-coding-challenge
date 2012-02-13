f54de2a1b212c43ef8e45cd4faf373e1aa39fe37
Peter Lyons <pete@peterlyons.com>
2012-02-13 12:02:04 -0700

    private makeView function

--- public/javascripts/lib/backbone.list.js
+++ public/javascripts/lib/backbone.list.js
@@ -16,6 +16,73 @@ function listTag() {
   return /^(ol|li)$/i.test(this.tagName);
 }
 
+function makeView(model) {
+  var viewClass = this.options.itemType || Backbone.View;
+  var itemOptions = _.defaults(this.options.itemOptions || {}, {model: model});
+  var viewTagIsDiv = (itemOptions.tagName || "div").toLowerCase() === 'div';
+  if (listTag.apply(this) && viewTagIsDiv) {
+    itemOptions.tagName = 'li';
+  }
+  var view = new viewClass(itemOptions);
+  var _mid = mid(model);
+  if (_mid) {
+    this._viewCache.byId[_mid] = view;
+  }
+  if (model.cid) {
+    this._viewCache.byCid[model.cid] = view;
+  }
+  this._viewCache.byViewCid[view.cid] = view;
+  view.bind('all', function() {
+    this.trigger.apply(this, arguments);
+  }, this);
+  return view;
+}
+
+function shouldWrap(view) {
+  var viewIsLi = view.tagName.toLowerCase() === 'li';
+  return listTag.apply(this) && !viewIsLi;
+}
+
+function cached(model) {
+  //The test cases pass several different types of objects/values to
+  //this function. We must handle the following cases:
+  //Model instance: compare to view's model by id then cid
+  //Model instance who's id has been changed since we cached it
+  //Model id number
+  //Model cid string
+  //View cid string
+  if (model instanceof Backbone.Model) {
+    //Check id first, then cid as a fallback
+    var _mid = mid(model);
+    var view = this._viewCache.byId[_mid];
+    if (view) { return view; }
+    view = this._viewCache.byCid[model.cid];
+    if (view) { return view; }
+    return null;
+  } else {
+    //Treat as an id number or string
+    var caches = [this._viewCache.byId, this._viewCache.byCid,
+      this._viewCache.byViewCid ];
+     var cache = _.find(caches, function(cache) {
+      return cache[model];
+    });
+    if (cache) {
+      return cache[model];
+    }
+    //OK, last ditch is locate the model object in the collection by ID
+    //then find the cached view by object
+    var model = this.collection.find(function (m) {
+      return mid(m) == model;
+    }, this);
+    if (model) {
+      return _.find(_.values(this._viewCache.byViewCid), function(view) {
+        return view.model === model;
+      });
+    }
+    return null;
+  }
+}
+
 var List = Backbone.List = Backbone.View.extend({
 
   initialize: function(options) {
@@ -34,80 +101,11 @@ var List = Backbone.List = Backbone.View.extend({
     syncViews.apply(this);
   },
 
-  _makeView: function(model) {
-    var viewClass = this.options.itemType || Backbone.View;
-    var itemOptions = _.defaults(this.options.itemOptions || {}, {model: model});
-    var viewTagIsDiv = (itemOptions.tagName || "div").toLowerCase() === 'div';
-    if (listTag.apply(this) && viewTagIsDiv) {
-      itemOptions.tagName = 'li';
-    }
-    var view = new viewClass(itemOptions);
-    var _mid = mid(model);
-    if (_mid) {
-      this._viewCache.byId[_mid] = view;
-    }
-    if (model.cid) {
-      this._viewCache.byCid[model.cid] = view;
-    }
-    this._viewCache.byViewCid[view.cid] = view;
-    view.bind('all', function() {
-      this.trigger.apply(this, arguments);
-    }, this);
-    return view;
-  },
-
-  _cached: function(model) {
-    //The test cases pass several different types of objects/values to
-    //this function. We must handle the following cases:
-    //Model instance: compare to view's model by id then cid
-    //Model instance who's id has been changed since we cached it
-    //Model id number
-    //Model cid string
-    //View cid string
-    if (model instanceof Backbone.Model) {
-      //Check id first, then cid as a fallback
-      var _mid = mid(model);
-      var view = this._viewCache.byId[_mid];
-      if (view) { return view; }
-      view = this._viewCache.byCid[model.cid];
-      if (view) { return view; }
-      return null;
-    } else {
-      //Treat as an id number or string
-      var caches = [this._viewCache.byId, this._viewCache.byCid,
-        this._viewCache.byViewCid ];
-       var cache = _.find(caches, function(cache) {
-        return cache[model];
-      });
-      if (cache) {
-        return cache[model];
-      }
-      //OK, last ditch is locate the model object in the collection by ID
-      //then find the cached view by object
-      var model = this.collection.find(function (m) {
-        return mid(m) == model;
-      }, this);
-      if (model) {
-        return _.find(_.values(this._viewCache.byViewCid), function(view) {
-          return view.model === model;
-        });
-      }
-      return null;
-    }
-  },
-
-
-
-  _shouldWrap: function(view) {
-    var viewIsLi = view.tagName.toLowerCase() === 'li';
-    return listTag.apply(this) && !viewIsLi;
-  },
-
   findView: function(model) {
-    var view = this._cached(model);
+    var view = cached.call(this, model);
     if (!view && this.collection.include(model)) {
       //Build a new view on demand
-      return this._makeView(model);
+      return makeView.call(this, model);
     }
     //Don't find views that are not currently mapped to the collection
     if (_.include(this.views, view)) {
@@ -122,7 +120,7 @@ var List = Backbone.List = Backbone.View.extend({
     this.collection.each(function(model) {
       var view = this.findView(model);
       var viewEl = view.render().el;
-      if (this._shouldWrap(view)) {
+      if (shouldWrap.call(this, view)) {
         this.$el.append($("<li>").html(viewEl));
       } else {
         this.$el.append(viewEl);
