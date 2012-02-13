4ec80c92dfcee37746e03aa1cf4edb0d9e239c6e
Peter Lyons <pete@peterlyons.com>
2012-02-13 09:16:54 -0700

    implemented view caches by id, cid, and viewCid. 21/33 passing.

--- public/javascripts/lib/backbone.list.js
+++ public/javascripts/lib/backbone.list.js
@@ -1,44 +1,90 @@
 var List = Backbone.List = Backbone.View.extend({
 
   initialize: function(options) {
-    this.collection.bind('add', function(model) {
-      this.views.push(this._viewFor(model));
-    }, this);
-    this.views = _.map(this.collection.models, this._viewFor, this);
+    this._viewCache = {byId: {}, byCid: {}, byViewCid: {}};
+    this.collection.bind('add', this._syncViews, this);
+    this.collection.bind('remove', this._syncViews, this);
+    this._syncViews();
   },
 
-  _viewFor: function(model) {
+  _syncViews: function() {
+    this.views = this.collection.map(this.findView, this);
+    this.render();
+  },
+
+  _mid: function(model) {
+    return model.get(model.idAttribute);
+  },
+
+  _makeView: function(model) {
     var viewClass = this.options.itemType || Backbone.View;
-    return new viewClass({model: model});
+    var view = new viewClass({model: model});
+    var mid = this._mid(model);
+    if (mid) {
+      this._viewCache.byId[mid] = view;
+    }
+    if (model.cid) {
+      this._viewCache.byCid[model.cid] = view;
+    }
+    this._viewCache.byViewCid[view.cid] = view;
+    return view;
+  },
+
+  _cached: function(model) {
+    //The test cases pass several different types of objects/values to
+    //this function. We must handle the following cases:
+    //Model instance: compare to view's model by id then cid
+    //Model instance who's id has been changed since we cached it
+    //Model id number
+    //Model cid string
+    //View cid string
+    if (model instanceof Backbone.Model) {
+      //Check id first, then cid as a fallback
+      var mid = this._mid(model);
+      var view = this._viewCache.byId[mid];
+      if (view) { return view; }
+      view = this._viewCache.byCid[model.cid];
+      if (view) { return view; }
+      return null;
+    } else {
+      //Treat as an id number or string
+      var caches = [this._viewCache.byId, this._viewCache.byCid,
+        this._viewCache.byViewCid ];
+       var cache = _.find(caches, function(cache) {
+        return cache[model];
+      });
+      if (cache) {
+        return cache[model];
+      }
+      //OK, last ditch is locate the model object in the collection by ID
+      //then find the cached view by object
+      var model = this.collection.find(function (m) {
+        return this._mid(m) == model;
+      }, this);
+      if (model) {
+        return _.find(_.values(this._viewCache.byViewCid), function(view) {
+          return view.model === model;
+        });
+      }
+      return null;
+    }
   },
 
   findView: function(model) {
-    return _.find(this.views, function(view) {
-      //The test cases pass several different types of objects/values to
-      //this function. We must handle the following cases:
-      //Model instance: compare to view's model by id then cid
-      //Model id number
-      //Model cid string
-      //View cid string
-      var viewModelId = view.model.get(view.model.idAttribute);
-      if (typeof(model) === 'object') {
-        //treat as Model instace
-        return (model.id == viewModelId) || (model.cid == view.model.cid);
-      } else {
-        //Treat as an id number or string
-        return _.find([viewModelId, view.model.cid, view.cid], function (it) {
-          return it == model;
-        });
-      }
-    });
+    var view = this._cached(model);
+    if (!view && this.collection.include(model)) {
+      //Build a new view on demand
+      return this._makeView(model);
+    }
+    return view;
   },
 
   render: function() {
-    //var $ = this.$;
     var $container = $(this.el);
-    _.each(this.views, function(view) {
-      $container.append(view.render().el);
-    });
+    $container.html('');
+    this.collection.each(function(model) {
+      $container.append(this.findView(model).render().el);
+    }, this);
     return this;
   },
 
