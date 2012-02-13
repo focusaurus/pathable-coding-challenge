db0eed2e09235343466617144d30bf541fd9430d
Peter Lyons <pete@peterlyons.com>
2012-02-12 22:17:56 -0700

    findView and tracking model additions to the collection implemented well enough for 2 tests to pass

--- public/javascripts/lib/backbone.has.js
+++ public/javascripts/lib/backbone.has.js
@@ -612,15 +612,35 @@ var Model = Has.Model = Backbone.Model.extend({
 var List = Backbone.List = Backbone.View.extend({
 
   initialize: function(options) {
-    this.views = _.map(this.collection.models, function(model) {
-      return new Backbone.View({model: model, tagName: 'li'});
-    });
+    this.collection.bind('add', function(model) {
+      this.views.push(this._viewFor(model));
+    }, this);
+    this.views = _.map(this.collection.models, this._viewFor, this);
+  },
+
+  _viewFor: function(model) {
+
+    return new Backbone.View({model: model, tagName: 'li'});
   },
 
   findView: function(model) {
     return _.find(this.views, function(view) {
-      //TODO what is the most correct identity match here?
-      return view.model.id == model.id
+      //The test cases pass several different types of objects/values to
+      //this function. We must handle the following cases:
+      //Model instance: compare to view's model by id then cid
+      //Model id number
+      //Model cid string
+      //View cid string
+      var viewModelId = view.model.get(view.model.idAttribute);
+      if (typeof(model) === 'object') {
+        //treat as Model instace
+        return (model.id == viewModelId) || (model.cid == view.model.cid);
+      } else {
+        //Treat as an id number or string
+        return _.find([viewModelId, view.model.cid, view.cid], function (it) {
+          return it == model;
+        });
+      }
     });
   },
 
@@ -630,7 +650,6 @@ var List = Backbone.List = Backbone.View.extend({
     _.each(this.views, function(view) {
       $container.append(view.render().el);
     });
-    console.log("BUGBUG $container", $container);
     return this;
   },
 
